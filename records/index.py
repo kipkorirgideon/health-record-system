a44c90261d556eb2d03b5aabe69e5b0a88c698f2
kipkorirgideon <korirgideon29@gmail.com>
2022-11-20 05:43:05 +0300

    improve ui

--- records/index.py
+++ records/index.py
@@ -8,8 +8,8 @@ from .models import Patient
 class PatientIndex(AlgoliaIndex):
     should_index = 'is_active'
     custom_objectID = 'uuid'
-    fields = ('uuid', 'id', 'patient_id_number', 'first_name', 'last_name','date_of_birth', 'patient_id_number', 'age','ward')
+    fields = ('uuid', 'patient_id_number', 'first_name', 'last_name', 'patient_id_number', 'age', 'ward')
     settings = {
-        'searchableAttributes': ['searchable(patient_id_number)', 'searchable(ward)'],
-        'attributesForFaceting': ['first_name', 'last_name', 'ward'],
+        'searchableAttributes': ['patient_id_number', 'first_name', 'last_name', 'ward'],
+        'attributesForFaceting': ['searchable(ward)', 'age'],
     }
