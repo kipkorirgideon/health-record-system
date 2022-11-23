b7c920d97217b54e15c1f24c510dfd2bb00cc5e1
kipkorirgideon <korirgideon29@gmail.com>
2022-11-24 00:30:35 +0300

    improve style

--- accounts/views.py
+++ accounts/views.py
@@ -5,20 +5,11 @@ import mixins
 from records.models import PatientRecord
 
 
-class UserTypeRedirectView(generic.RedirectView):
+class UserTypeRedirectView(mixins.LoginRequiredMixin, generic.RedirectView):
     permanent = False
 
     def get_redirect_url(self, *args, **kwargs):
-        user = self.request.user
-        if user.is_superuser:
-            return reverse_lazy('home')
-        if user.user_type == user.USER_TYPE_DOCTOR or \
-                user.user_type == user.USER_TYPE_LAB_TECHNICIAN:
-            return reverse_lazy('patient_list')
-        elif user.user_type == user.USER_TYPE_CASHIER:
-            return reverse_lazy('home')
-        else:
-            return reverse_lazy('home')
+        return reverse_lazy('dashboard')
 
 
 class DashboardView(mixins.LoginRequiredMixin, generic.TemplateView):
